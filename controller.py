024bf5325f07e48a6feefe2796f53bbee151bd44
Zakhar <seriy.zachar@yandex.by>
2022-12-29 19:55:30 +0300

    ready add() and edit()

--- controller.py
+++ controller.py
@@ -8,6 +8,20 @@ def button():
     window.mainloop()
 
 
-def add(labels, entries, button_ok):
+def add(labels, entries, buttons):
     add_contact(entries)
-    change_visibility(labels, entries, button_ok, False)
+    change_visibility(labels, entries, buttons, False)
+    show_list(funcs.show_list())
+
+
+def edit(labels, entries, buttons):
+    table = show_list(funcs.show_list())
+    data = table.focus()
+    print(data)
+    item = table.item(data)
+    contact = item["values"]
+    contact = [str(i) for i in contact]
+    index = funcs.find_contact(contact)
+    funcs.edit_contact(index, entries)
+    change_visibility(labels, entries, buttons, False)
+    show_list(funcs.show_list())
